d90ad6d02770de97e16c78df191070d18156f379
annt <annt598@viettel.com.vn>
2020-12-10 11:30:20 +0700

    Updatecode

---
+++ DemoPackageFW/DemoPackageFW.h
@@ -0,0 +1,19 @@
+//
+//  DemoPackageFW.h
+//  DemoPackageFW
+//
+//  Created by annt on 12/10/20.
+//  Copyright © 2020 annt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoPackageFW.
+FOUNDATION_EXPORT double DemoPackageFWVersionNumber;
+
+//! Project version string for DemoPackageFW.
+FOUNDATION_EXPORT const unsigned char DemoPackageFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoPackageFW/PublicHeader.h>
+
+
---
+++ DemoPackageFW/Package.swift
@@ -0,0 +1,16 @@
+//
+//  Package.swift
+//  DemoPackageFW
+//
+//  Created by annt on 12/10/20.
+//  Copyright © 2020 annt. All rights reserved.
+//
+
+import Foundation
+public class Package {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
